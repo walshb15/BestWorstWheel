ba1281a4146a6dd8b8d45cbe57b743f03ecf346f
Brendan Walsh <walshb15@msu.edu>
2020-06-25 16:44:52 -0400

    Switch to pygame
    That generic graphics module doesn't seem to have a way to update the screen, so I'm going to try and use pygame for this project instead.

--- wheel.py
+++ wheel.py
@@ -1,17 +1,27 @@
-from graphics import *
+import pygame
 from math import sin, cos, atan2, pi, radians
 
 
 def main():
-    win = GraphWin("Wheel of the Worst!", 700, 700)
-
-    pt = Point(win.getHeight() / 2, win.getWidth() / 2)
-
-    radius = win.getWidth() / 3
-    cir = Circle(pt, radius)
-    cir.draw(win)
+    screen = pygame.display.set_mode((700, 700))
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    screen.fill(white)
+    surface = pygame.display.get_surface()
+    width, height = surface.get_size()
+    center = (int(width / 2), int(height / 2))
+    radius = int(width / 3)
+    circle = pygame.draw.circle(surface, black, center, radius, 1)
     angle = radians(0)
-    line = Line(pt, Point(pt.getX() + radius * cos(angle), pt.getY() + radius * sin(-angle)))
-    line.draw(win)
+    endPoint = (center[0] + radius * cos(angle), center[1] + radius * sin(-angle))
+    line = pygame.draw.line(surface, black, center, endPoint)
+    pygame.display.flip()
+    pygame.display.set_caption("Wheel of the Worst!")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                running = False
 
 main()
